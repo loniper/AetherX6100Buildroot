67318cbd4361e8488615c9c08ed7684d1a1b661d
loniper <loniper@me.com>
2025-11-11 18:04:59 +0800

    Update x6100_gui.mk

--- br2_external/package/x6100-gui/x6100_gui.mk
+++ br2_external/package/x6100-gui/x6100_gui.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-X6100_GUI_VERSION = v0.23.0-rc.3
-X6100_GUI_SITE = https://github.com/gdyuldin/x6100_gui
+# X6100_GUI_VERSION = v0.23.0-rc.3
+X6100_GUI_SITE = https://github.com/loniper/x6100_gui
 X6100_GUI_SITE_METHOD = git
 X6100_GUI_GIT_SUBMODULES = YES
 X6100_GUI_LICENSE = GPLv2
